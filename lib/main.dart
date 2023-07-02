fa79327e3304ae961bff92f73a13516709c606a7
lahirujay99 <lahirujayawardana94@gmail.com>
2023-07-02 22:58:44 +0530

    added backgorund color

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.indigoAccent, //can also use Color()
         body: Center(
           child: Text("Hello World"),
         ),
